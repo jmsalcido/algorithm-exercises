7f31c6cafe5ec101d61f46a201a098d349c0fc1d
jsalcido <jsalcido@switchfly.com>
2014-05-31 23:34:51 -0700

    fast refactor

--- algorithms/src/main/java/org/otfusion/sorting/InsertionSort.java
+++ algorithms/src/main/java/org/otfusion/sorting/InsertionSort.java
@@ -2,30 +2,37 @@ package org.otfusion.sorting;
 
 import java.util.List;
 
+/**
+ * Insertion Sort is a sorting algorithm O(n) that sorts elements like a hand of cards.
+ */
 public class InsertionSort implements Sort {
 
     public void sort(List<Integer> listToSort) {
+        insertionSort(listToSort, false);
+    }
+
+    private void insertionSort(List<Integer> listToSort, boolean revert) {
         for(int j = 1; j < listToSort.size(); j++) {
             Integer key = listToSort.get(j);
             Integer i = j - 1;
-            while(i >= 0 && listToSort.get(i) > key) {
+            while(i >= 0 && compare(listToSort.get(i), key, revert)) {
                 listToSort.set(i+1, listToSort.get(i));
                 i = i -1;
             }
-            listToSort.set(i+1, key);
+            listToSort.set(i + 1, key);
+        }
+    }
+
+    private boolean compare(Integer key, Integer element, boolean revert) {
+        if(revert) {
+            return element > key;
+        } else {
+            return element < key;
         }
     }
 
     @Override
     public void sortDecreased(List<Integer> listToSort) {
-        for(int j = 1; j < listToSort.size(); j++) {
-            Integer key = listToSort.get(j);
-            Integer i = j - 1;
-            while(i >= 0 && listToSort.get(i) < key) {
-                listToSort.set(i+1, listToSort.get(i));
-                i = i -1;
-            }
-            listToSort.set(i+1, key);
-        }
+        insertionSort(listToSort, true);
     }
 }
