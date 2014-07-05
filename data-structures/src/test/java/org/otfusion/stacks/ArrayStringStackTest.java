ea3c25f0d401c2c41ec79a3481e613f6b2e3bc00
jsalcido <jsalcido@switchfly.com>
2014-07-05 12:56:04 -0700

    added stack overflow example

--- data-structures/src/test/java/org/otfusion/stacks/ArrayStringStackTest.java
+++ data-structures/src/test/java/org/otfusion/stacks/ArrayStringStackTest.java
@@ -3,6 +3,8 @@ package org.otfusion.stacks;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 public class ArrayStringStackTest {
     @Test
@@ -13,11 +15,45 @@ public class ArrayStringStackTest {
         stack.push("are");
         stack.push("you");
         stack.push("?");
-        assertEquals(5,stack.size());
+        assertEquals(5, stack.size());
         assertEquals("?", stack.pop());
         assertEquals("you", stack.pop());
         assertEquals("are", stack.pop());
         assertEquals("how", stack.pop());
         assertEquals("Hello", stack.pop());
+    }
+
+    @Test
+    public void testStackWithStackOverflow() throws Exception {
+        try {
+            // Size = 10, only 10 spaces for the array
+            ArrayStringStack stack = new ArrayStringStack(10);
+            stack.push("Hello");
+            stack.push("how");
+            stack.push("are");
+            stack.push("you");
+            stack.push("?");
+            stack.push("?");
+            stack.push("?");
+            stack.push("?");
+            stack.push("?");
+            stack.push("?");
+            stack.push("?");
+            assertEquals(11, stack.size());
+            assertEquals("?", stack.pop());
+            assertEquals("?", stack.pop());
+            assertEquals("?", stack.pop());
+            assertEquals("?", stack.pop());
+            assertEquals("?", stack.pop());
+            assertEquals("?", stack.pop());
+            assertEquals("?", stack.pop());
+            assertEquals("you", stack.pop());
+            assertEquals("are", stack.pop());
+            assertEquals("how", stack.pop());
+            assertEquals("Hello", stack.pop());
+            fail("No excpetion thrown");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            assertTrue(true);
+        }
     }
 }
