1a25c42b307a9c5a79bd1a641a83c0dd4148cadc
jmsalcido <jose@otfusion.org>
2018-01-20 19:33:15 -0700

    movedStrings

---
+++ 2017/java/src/main/java/study/Strings.java
@@ -0,0 +1,11 @@
+package study;
+
+public class Strings {
+
+    // stupid interview questions... - . -
+    public boolean isStringMoved(String input, String otherString) {
+        return (otherString + otherString).contains(input);
+    }
+
+
+}
---
+++ 2017/java/src/test/java/study/StringsSpec.groovy
@@ -0,0 +1,22 @@
+package study
+
+import spock.lang.Specification
+import spock.lang.Unroll
+
+@Unroll
+class StringsSpec extends Specification {
+
+    def "isStringMoved() scenarios: #input is moved: #otherString"() {
+        def instance = Strings.newInstance()
+
+        expect:
+        assert instance.isStringMoved(input, otherString)
+
+        where:
+        input    | otherString | output
+        "string" | "trings"    | true
+        "string" | "tringss"   | false
+        "string" | "ringst"   | false
+    }
+
+}
